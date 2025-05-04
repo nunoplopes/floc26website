b6dab130e1690cae9f27152a12700586ee83b6a8
Markprnce115 <markprince29.mp@gmail.com>
2025-05-04 16:57:51 +0100

    fixed some bugs

--- components/event data/speakers.js
+++ components/event data/speakers.js
@@ -1,31 +1,30 @@
-import GIUSEPPE from '@/assets/images/giuseppe.jpg'; // Corrected import path
-import IMAGE2 from '@/assets/images/isil.jpg';     // Corrected import path
-import IMAGE3 from '@/assets/images/leonardo.jpg'; // Corrected import path
-
+import IMAGE1 from '../../assets/images/Giuseppe.jpg';
+import IMAGE2 from '@/assets/images/isil.jpg';
+import IMAGE3 from '@/assets/images/leonardo.jpg';
 
 export const keyNoteSpeakers = [
     {
         id: 1,
         name: "Giuseppe De Diacomo",
-        image: GIUSEPPE,
+        image: IMAGE1,
         position: "Professor of computer science at the department of of the university of Oxford...",
         description: "Giuseppe De Giacomo is a Professor of Computer Science at the Department of Computer Science of the University of Oxford. He was previously a Professor at the Department of Computer, Control and Management Engineering of the University of Roma 'La Sapienza'. His research activity concerns theoretical, methodological, and practical aspects in different areas of AI and CS, most prominently Knowledge Representation, Reasoning about Actions, Generalized Planning, Autonomous Agents, Reactive Synthesis and Verification, Service Composition, Business Process Modeling, and Data Management and Integration. He is an AAAI Fellow, ACM Fellow, and EurAI Fellow. He received an ERC Advanced Grant for the project WhiteMech: White-box Self Programming Mechanisms. He was the Program Chair of ECAI 2020 and KR 2014. He is on the Board of EurAI.",
-        link: "link: https://www.diag.uniroma1.it/~degiacom/"
+        link: "https://www.diag.uniroma1.it/~degiacom/"
     },
     {
         id: 2,
         name: "Isil Dilling",
         image: IMAGE2,
         position: "Professor of Computer Science at the university of Texas...",
-        description: "Isil Dillig (PhD, Stanford) is a Professor of Computer Science at the University of Texas at Austin where she leads the UToPiA research group.  Her primary research areas are programming languages and formal methods, with an emphasis on software verification and program synthesis.  She has published many academic papers at top conferences in her field (PLDI, POPL, OOPSLA, CAV etc), several of which have received best and distinguished paper awards. She was a recipient of an NSF CAREER Award in 2015 and was also selected as a Sloan Fellow in the same year. She has been a PI (or a co-PI) on several grants from the National Science Foundation and DARPA. She has also been a program committee chair for several conferences, including OOPSLA'26, PLDI'22 and CAV'19.  She is also the recipient of several teaching awards at UT Austin, including the Texas 10 award and the College of Natural Sciences Teaching Excellence Awar",
-        link: "link: https://www.cs.utexas.edu/~isil/"
+        description: "Isil Dillig (PhD, Stanford) is a Professor of Computer Science at the University of Texas at Austin where she leads the UToPiA research group.  Her primary research areas are programming languages and formal methods, with an emphasis on software verification and program synthesis.  She has published many academic papers at top conferences in her field (PLDI, POPL, OOPSLA, CAV etc), several of which have received best and distinguished paper awards. She was a recipient of an NSF CAREER Award in 2015 and was also selected as a Sloan Fellow in the same year. She has been a PI (or a co-PI) on several grants from the National Science Foundation and DARPA. She has also been a program committee chair for several conferences, including OOPSLA'26, PLDI'22 and CAV'19.  She is also the recipient of several teaching awards at UT Austin, including the Texas 10 award and the College of Natural Sciences Teaching Excellence Award.",
+        link: "https://www.cs.utexas.edu/~isil/"
     },
     {
         id: 3,
         name: "Leonardo de Moura",
         image: IMAGE3,
-        position: "Senior Principal and Applied Scientist in the Automated Reasonng Group at AWS...",
+        position: "Senior Principal and Applied Scientist in the Automated Reasoning Group at AWS...",
         description: "Leo is a Senior Principal Applied Scientist in the Automated Reasoning Group at AWS. In his spare time, he dedicates himself to serving as the Chief Architect of the Lean FRO, a non-profit organization that he proudly co-founded alongside Sebastian Ullrich. He is also honored to hold a position on the Board of Directors at the Lean FRO, where he actively contributes to its growth and development. Before joining AWS in 2023, he was a Senior Principal Researcher in the RiSE group at Microsoft Research, where he worked for 17 years starting in 2006. Prior to that, he worked as a Computer Scientist at SRI International. His research areas are automated reasoning, theorem proving, decision procedures, SAT and SMT. He is the main architect of several automated reasoning tools: Lean, Z3, Yices 1.0 and SAL. Leo’s work in automated reasoning has been acknowledged with a series of prestigious awards, including the CAV, Haifa, and Herbrand awards, as well as the Programming Languages Software Award by the ACM. Leo’s work has also been reported in the New York Times and many popular science magazines such as Wired, Quanta, and Nature News.",
-        link: "link: https://leodemoura.github.io"
+        link: "https://leodemoura.github.io"
     }
 ];
