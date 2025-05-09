d7336ed6c045fe7ad2b9c1da3d1abea37870b8a2
Markprnce115 <markprince29.mp@gmail.com>
2025-05-09 02:24:24 +0100

    fixed some bugs

--- app/speakers/[id]/page.jsx
+++ app/speakers/[id]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import { useParams } from 'next/navigation'
-import { motion } from 'framer-motion'
+import { motion} from 'framer-motion'
 import Link from 'next/link'
 import { keyNoteSpeakers } from '@/components/event data/speakers'
 
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
             'sqigmath.tecnico.ulisboa.pt'
         ], // Add the external domain here
     },
+    webpack: (config) => {
+        config.resolve.extensions.push(".mjs");
+        return config;
+    },
 };
 
 export default nextConfig;
