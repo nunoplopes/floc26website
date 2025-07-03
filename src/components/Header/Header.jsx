0509a255a1e4d648a1585767554b8b2eda54baf3
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-07-03 17:37:52 +0100

    switch to svg logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
-import LOGO from "../../assets/images/logo-small.png";
+import { useState } from "react";
+import LOGO from "../../assets/images/logo.svg";
 import { Link } from "react-router-dom";
 import { HiMenu, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const handleLinkClick = () => setIsMenuOpen(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const navLinks = [
+    { to: "/", label: "Home" },
+    { to: "/about", label: "About" },
+    { to: "/registration", label: "Registration" },
+    { to: "/venue", label: "Venue" },
+    { to: "/transportation", label: "Transportation" },
+    { to: "/committees", label: "Committees" },
+    { to: "/program", label: "Program" },
+    // { to: "/accommodation", label: "Accommodation" },
+    // { to: "/tourism", label: "Tourism" },
+  ];
 
   return (
     <header className="flex items-center justify-between z-10 p-5 bg-white">
-      {/* logo */}
       <div>
         <Link to="/">
           <img src={LOGO} width={100} height={100} alt="logo icon" />
         </Link>
       </div>
 
-      {/* nav links */}
       <motion.nav
         className={`
           text-sm font-bold
@@ -36,30 +44,11 @@ const Header = () => {
         transition={{ duration: 0.5, delay: 0.7, ease: "easeOut" }}
       >
         <motion.ul className="flex flex-col md:flex-row gap-4 md:gap-8 mt-16 md:mt-0">
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/about">
-            <li>About</li>
-          </Link>
-          <Link to="/registration">
-            <li>Registration</li>
-          </Link>
-          <Link to="/venue">
-            <li>Venue</li>
-          </Link>
-          <Link to="/transportation">
-            <li>Transportation</li>
-          </Link>
-          {/*<Link to='/speakers'><li>Keynote Speakers</li></Link> */}
-          <Link to="/committees">
-            <li>Committees</li>
-          </Link>
-          <Link to="/program">
-            <li>Program</li>
-          </Link>
-          {/* <Link to='/accomodation'><li>Accomodation</li></Link>  */}
-          {/* <Link to='/tourism'><li>Tourism</li></Link> */}
+          {navLinks.map(({ to, label }) => (
+            <Link key={to} to={to} onClick={handleLinkClick}>
+              <li>{label}</li>
+            </Link>
+          ))}
         </motion.ul>
       </motion.nav>
 
