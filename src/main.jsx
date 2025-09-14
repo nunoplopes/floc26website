a176348a7a86e58626b813cab7e22467d7047e1b
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-09-14 14:42:12 +0100

    smooth scroll to top on link click

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from "react";
+import { StrictMode, useLayoutEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import { BrowserRouter as Router } from "react-router";
+import { BrowserRouter as Router, useLocation } from "react-router";
 import Header from "./components/Header/Header.jsx";
 import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/400.css";
@@ -11,9 +11,21 @@ import "@fontsource/montserrat/600.css";
 import "@fontsource/montserrat/700.css";
 import "./index.css";
 
+const ScrollToTop = () => {
+  useLayoutEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [useLocation()]);
+  return null;
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
+      <ScrollToTop />
       <Header />
       <App />
       <Footer />
